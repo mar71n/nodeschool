2a6872f0236cfad4b97004c9b679ff342fe3a771
mar71n <mar71n@gmail.com>
2017-01-27 11:03:47 -0300

    learnyounode baby_steps

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,1 +1,8 @@
-console.log('HELLO WORLD');
+// console.log('HELLO WORLD');
+// console.log(process.argv);
+// console.log(process.argv.length);
+var resultado = 0;
+for (i=2; i < process.argv.length; i++){
+  resultado += Number(process.argv[i]);
+}
+console.log(resultado);
